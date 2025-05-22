fd2af763861eb8e762c8ed84d0e37b790157e0bd
hoanhua14 <huasohoan@gmail.com>
2025-05-21 18:35:31 -0700

    used reusable component ProductCard to display each product within Products dynamically

--- 168/src/components/ProductCard.jsx
+++ 168/src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-const ProductCard = () => {
-  return <>each product</>;
+const ProductCard = ({ name }) => {
+  return <div>{name}</div>;
 };
 export default ProductCard;
--- 168/src/components/Products.jsx
+++ 168/src/components/Products.jsx
@@ -1,4 +1,31 @@
+import ProductCard from "./ProductCard";
+
+const productList = {
+  Signatures: [
+    { id: 1, name: "Tiramisu" },
+    { id: 2, name: "Flan" },
+  ],
+  "Cream Puffs": [
+    { id: 3, name: "Vanilla" },
+    { id: 4, name: "Assorted" },
+  ],
+  Brownies: [
+    { id: 5, name: "Matcha" },
+    { id: 6, name: "Truyền Thống" },
+    { id: 7, name: "Oreo Cheese Cake" },
+  ],
+};
 const Products = ({ category }) => {
-  return <div>{category}</div>;
+  const products = productList[category];
+  return (
+    <div>
+      <div>{category}</div>
+      <div>
+        {products.map((product) => (
+          <ProductCard key={product.id} name={product.name} />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Products;
