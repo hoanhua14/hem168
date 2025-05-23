372993ab3b78a5e51b074b1aaae9f4423ef8c664
hoanhua14 <huasohoan@gmail.com>
2025-05-22 23:40:37 -0700

    added prices to each product in productList and helper function to format currency in ProductCard

--- 168/src/components/ProductCard.jsx
+++ 168/src/components/ProductCard.jsx
@@ -1,4 +1,11 @@
-const ProductCard = ({ name }) => {
-  return <div className="product-card">{name}</div>;
+const ProductCard = ({ name, price }) => {
+  const formatVND = (number) =>
+    number.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
+  return (
+    <div className="product-card">
+      {name}
+      <div>{formatVND(price)}</div>
+    </div>
+  );
 };
 export default ProductCard;
--- 168/src/components/Products.jsx
+++ 168/src/components/Products.jsx
@@ -2,17 +2,17 @@ import ProductCard from "./ProductCard";
 
 const productList = {
   Signatures: [
-    { id: 1, name: "Tiramisu" },
-    { id: 2, name: "Flan" },
+    { id: 1, name: "Tiramisu", price: 100000 },
+    { id: 2, name: "Flan", price: 55000 },
   ],
   "Cream Puffs": [
-    { id: 3, name: "Vanilla" },
-    { id: 4, name: "Assorted" },
+    { id: 3, name: "Vanilla", price: 72000 },
+    { id: 4, name: "Assorted", price: 77000 },
   ],
   Brownies: [
-    { id: 5, name: "Matcha" },
-    { id: 6, name: "Truyền Thống" },
-    { id: 7, name: "Oreo Cheese Cake" },
+    { id: 5, name: "Matcha", price: 55000 },
+    { id: 6, name: "Truyền Thống", price: 55000 },
+    { id: 7, name: "Oreo Cheese Cake", price: 55000 },
   ],
 };
 const Products = ({ category }) => {
@@ -23,7 +23,11 @@ const Products = ({ category }) => {
         {category}
         <div style={{ display: "flex" }}>
           {products.map((product) => (
-            <ProductCard key={product.id} name={product.name} />
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+            />
           ))}
         </div>
       </div>
