5dc1e717f780342e684bbcf2f7bb418543f1c3ca
hoanhua14 <huasohoan@gmail.com>
2025-06-02 14:48:15 -0700

    wrapped app inside Browser Router, created order summary route and component

--- 168/src/main.jsx
+++ 168/src/main.jsx
@@ -2,10 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Summary from "./components/Summary.jsx";
 import { Provider } from "react-redux";
 import store from "./store.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/summary" element={<Summary />}></Route>
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
