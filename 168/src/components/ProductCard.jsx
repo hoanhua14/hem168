f265d12da3cd6715a784be3f18830a8906b92dd0
hoanhua14 <huasohoan@gmail.com>
2025-06-02 18:43:51 -0700

    added shown quantity on the order page

--- 168/src/components/ProductCard.jsx
+++ 168/src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import { addItem } from "../features/cartSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 const ProductCard = ({ name, price, id }) => {
   const formatVND = (number) =>
     number.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
@@ -7,10 +7,18 @@ const ProductCard = ({ name, price, id }) => {
   const handleAddToCart = () => {
     dispatch(addItem({ name, price, id }));
   };
+  const quantityOfAProduct = useSelector((state) => {
+    const itemToFind = state.cart.items.find((item) => item.id === id);
+    if (itemToFind) {
+      return itemToFind.quantity;
+    }
+  });
+
   return (
     <div className="product-card">
       {name}
       <div>{formatVND(price)}</div>
+      <div>Quantity: {quantityOfAProduct}</div>
       <button onClick={handleAddToCart}>Add</button>
     </div>
   );
