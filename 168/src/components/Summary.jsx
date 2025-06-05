3b3b4b65c274c5334e7517206534a5250776b78e
hoanhua14 <huasohoan@gmail.com>
2025-06-04 20:38:17 -0700

    basic functionalities done

--- 168/src/components/Summary.jsx
+++ 168/src/components/Summary.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import Header from "./Header";
 import { addItem, removeItem } from "../features/cartSlice";
+import EmptyCart from "./EmptyCart";
 export default function Summary() {
   const formatVND = (number) =>
     number.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
@@ -11,36 +12,42 @@ export default function Summary() {
     (state) => state.user
   );
   return (
-    <>
+    <div>
       <Header />
-      <div>
-        <h2>Order Summary</h2>
-        <p>
-          <strong>Name:</strong> {name}
-        </p>
-        <p>
-          <strong>Address:</strong> {add}
-        </p>
-        <p>
-          <strong>Phone:</strong> {phone}
-        </p>
-        <p>
-          <strong>Delivery Time:</strong> {deliveryTime}
-        </p>
-        <p>
-          <strong>Note:</strong> {note}
-        </p>
-      </div>
-      <div>
-        {items.map((item, index) => (
-          <h1 key={index}>
-            {item.name}, {formatVND(item.price)}, {item.quantity},
-            <button onClick={() => dispatch(addItem(item))}>+</button>
-            <button onClick={() => dispatch(removeItem(item))}>-</button>
-          </h1>
-        ))}
-      </div>
-      <div>Total is {formatVND(totalPrice + 30000)}</div>
-    </>
+      {items.length === 0 ? (
+        <EmptyCart />
+      ) : (
+        <>
+          <div>
+            <h2>Order Summary</h2>
+            <p>
+              <strong>Name:</strong> {name}
+            </p>
+            <p>
+              <strong>Address:</strong> {add}
+            </p>
+            <p>
+              <strong>Phone:</strong> {phone}
+            </p>
+            <p>
+              <strong>Delivery Time:</strong> {deliveryTime}
+            </p>
+            <p>
+              <strong>Note:</strong> {note}
+            </p>
+          </div>
+          <div>
+            {items.map((item, index) => (
+              <h1 key={index}>
+                {item.name}, {formatVND(item.price)}, {item.quantity},
+                <button onClick={() => dispatch(addItem(item))}>+</button>
+                <button onClick={() => dispatch(removeItem(item))}>-</button>
+              </h1>
+            ))}
+          </div>
+          <div>Total is {formatVND(totalPrice + 30000)}</div>
+        </>
+      )}
+    </div>
   );
 }
