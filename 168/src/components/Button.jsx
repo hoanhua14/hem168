afb95a243acb78ef2c09fb19a2ba230e6b31789d
hoanhua14 <huasohoan@gmail.com>
2025-06-12 17:41:27 -0700

    finished styling UserInput

--- 168/src/components/Button.jsx
+++
@@ -1,4 +0,0 @@
-const Button = () => {
-  return <button>add</button>;
-};
-export default Button;
--- 168/src/components/UserInfo.jsx
+++ 168/src/components/UserInfo.jsx
@@ -27,62 +27,65 @@ export default function UserInfo() {
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="form">
-        <label>
-          Name:
-          <input
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Address:
-          <input
-            value={add}
-            onChange={(e) => setAdd(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Phone:
-          <input
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Delivery Time:
-          <input
-            type="datetime-local"
-            min={getCurrentDateTimeLocal()}
-            value={deliveryTime}
-            onChange={(e) => setDeliveryTime(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label
+        <div className="form-details">
+          <label>
+            Name:
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            Address:
+            <input
+              value={add}
+              onChange={(e) => setAdd(e.target.value)}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            Phone:
+            <input
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            Delivery Time:
+            <input
+              type="datetime-local"
+              min={getCurrentDateTimeLocal()}
+              value={deliveryTime}
+              onChange={(e) => setDeliveryTime(e.target.value)}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            <span>Note:</span>
+            <textarea
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+              rows={4}
+            />
+          </label>
+          <br />
+        </div>
+        <div
           style={{
+            justifyContent: "center",
             display: "flex",
-            flexDirection: "row",
-            alignItems: "flex-start",
           }}
         >
-          Note:
-          <textarea
-            value={note}
-            onChange={(e) => setNote(e.target.value)}
-            rows={4}
-          />
-        </label>
-        <br />
-        <button type="submit" className="add-button">
-          checkout
-        </button>
+          <button type="submit" className="add-button">
+            Check Out
+          </button>
+        </div>
       </form>
     </div>
   );
